915356b22d1ce99635a076374173a20f60cf08fa
simonegreco5 <simoxey.greco@gmail.com>
2026-06-29 18:57:42 +0200

    - add MILESTONE 2

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,18 +1,29 @@
 const posts = require('../data/posts');
+const connection = require('../database/connection');
 
 const index = (req, res) => {
-    // res.json({ message: "show all posts" })
+    // // res.json({ message: "show all posts" })
 
-    console.log(req.query) // example: { tag: salati }
-    const tag = req.query.tag
+    // console.log(req.query) // example: { tag: salati }
+    // const tag = req.query.tag
 
-    if (tag) {
-        const filteredPost = posts.filter(post => post.tags.includes(tag))
-        return res.json(filteredPost)
-    }
-    // for try: http://localhost:3000/api/posts?tag=salati (?chiave=valore)
+    // if (tag) {
+    //     const filteredPost = posts.filter(post => post.tags.includes(tag))
+    //     return res.json(filteredPost)
+    // }
+    // // for try: http://localhost:3000/api/posts?tag=salati (?chiave=valore)
 
-    res.json(posts)
+    // res.json(posts)
+
+    const sql = 'SELECT * FROM posts';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).json({ error: true, message: "Internal Server Error"});
+        }
+        res.json(results);
+    })
 }
 
 const show = (req, res) => {
